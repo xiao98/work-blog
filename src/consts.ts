afc336626dc3b2a98afdba31bdfd512a47b00fa8
xiao98 <a964852064@gmail.com>
2026-05-02 17:39:21 +0200

    Initial commit: Hao's work blog (based on Astro Nano)

---
+++ src/consts.ts
@@ -0,0 +1,19 @@
+import type { Site, Metadata } from "@types";
+
+export const SITE: Site = {
+  NAME: "Hao的工作日志",
+  EMAIL: "",
+  NUM_POSTS_ON_HOMEPAGE: 10,
+  NUM_WORKS_ON_HOMEPAGE: 0,
+  NUM_PROJECTS_ON_HOMEPAGE: 0,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Hao的工作日志",
+  DESCRIPTION: "Hao 的博客。",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "Hao 的博客。",
+};
